50872a1fb2e42292a35bb29728dd559856853295
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2021-09-26 19:40:04 +0200

    Case 1, Case 2  & Case 3
    Except for the skill thing that i dont know what this is

--- Networking - Optional Exercice/Assets/Spawning.cs
+++ Networking - Optional Exercice/Assets/Spawning.cs
@@ -9,33 +9,72 @@ public class Spawning : MonoBehaviour
 
     private Vector3 spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
     float speed = 1f;
-    private bool isSpawned;
+
+    bool isCoroutineRunning = false;
 
     int maxSpawned = 5;
     int currentSpawned = 0;
     float lifeTime = 5f;
-    void Start()
-    {
-        isSpawned = false;
-    }
-
     
     void Update()
     {
 
-        
+        //CASE 1
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine("CASE_1");
+        }
+        //------------
+        //CASE 2
+        //if (Input.GetKeyDown(KeyCode.Mouse0) && !isCoroutineRunning)
+        //{
+        //    StartCoroutine("CASE_2");
+        //}
+        //--------------- 
 
         //CASE 3
-        if(Input.GetKeyDown(KeyCode.Mouse0) && currentSpawned < maxSpawned)
-        {
-            currentSpawned++;
-            StartCoroutine("ManageObj");
-        }
+        //if (Input.GetKeyDown(KeyCode.Mouse0) && currentSpawned < maxSpawned)
+        //{
+        //    currentSpawned++;
+        //    StartCoroutine("CASE_3");
+        //}
         //---------
     }
 
+    //CASE 1
+    IEnumerator CASE_1()
+    {
+        GameObject go = Instantiate(itemPrefab, spawnPosition, Quaternion.identity);
+        float time = 0;
+
+        while (time <= lifeTime)
+        {
+            go.transform.position += go.transform.forward * Time.deltaTime * speed;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(go);
+    }
+    //---------
+    //CASE 2
+    IEnumerator CASE_2()
+    {
+        isCoroutineRunning = true;
+        GameObject go = Instantiate(itemPrefab, spawnPosition, Quaternion.identity);
+        float time = 0;
+
+        while (time <= lifeTime)
+        {
+            go.transform.position += go.transform.forward * Time.deltaTime * speed;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(go);
+        isCoroutineRunning = false;
+    }
+    //---------
     //CASE 3
-    IEnumerator ManageObj()
+    IEnumerator CASE_3()
     {
         GameObject go =  Instantiate(itemPrefab, spawnPosition, Quaternion.identity);
         float time = 0;
@@ -50,28 +89,6 @@ public class Spawning : MonoBehaviour
         Destroy(go);
         
     }
-    //void moveObject()
-    //{
-    //    itemPrefab.transform.position += itemPrefab.transform.forward * Time.deltaTime * speed;
-    //}
 
-    //IEnumerator destroyPrefab()
-    //{
-    //    System.DateTime myTime = System.DateTime.UtcNow;
-    //    while (true)
-    //    {
-    //        while ((System.DateTime.UtcNow - myTime).Seconds < 5.0)
-    //        {
-    //            yield return null; //"si es null ejecuta frame y después vuelve a ver si he acabado."
-    //                               //Debug.Log(System.DataTime.UtcNow;
 
-    //        }
-
-    //        Destroy(itemPrefab);
-    //        Debug.Log("The object has been destroyed after 5 seconds");
-    //        isSpawned = false;
-
-    //        myTime = System.DateTime.UtcNow;
-    //    }
-    //}
 }
