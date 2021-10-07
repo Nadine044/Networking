6847ac0aae8fdd54983a0ef6d8692337fa34a852
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2021-10-07 20:45:06 +0200

    Ping Pong through UDP connection DONE!

--- TCP&UDP/Assets/Scripts/ClientUDP.cs
+++ TCP&UDP/Assets/Scripts/ClientUDP.cs
@@ -5,97 +5,81 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 public class ClientUDP : MonoBehaviour
 {
     // Start is called before the first frame update
     //IPAddress.Parse("127.0.0.1")
 
+    private Socket server;
+    byte[] data = new byte[1024];
 
-
-
+    ServerUDP serverUDP;
+    bool testing = false;
     void Start()
     {
-        //127.0.0.1
+         
+        //c.Client("127.0.0.1", 27000);
+        //startThreadingFunction(ExampleClient);
+    }
+
+    public void startThreadingFunction(Action function)
+    {
+        Thread t = new Thread(function.Invoke);
+        t.Start();
+    }
+
+
+
+    void ExampleClient()
+    {
+        int count = 0;
+        server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        EndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27000);
+
+        string ping = "ping";
+        count++;
+        data = Encoding.ASCII.GetBytes(ping);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)sender;
+
+        data = new byte[1024];
+        int recv = server.ReceiveFrom(data, ref Remote);
+
+        Debug.Log("Message recieved from " + Remote.ToString());
+        Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+
+        while (count <5)
+        {
+            count++;
+
+            server.SendTo(Encoding.ASCII.GetBytes(ping), Remote);
+            Thread.Sleep(500);
+            data = new byte[1024];
+            recv = server.ReceiveFrom(data, ref Remote);
+            Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+        }
+
+        //server.Close();
     }
 
     // Update is called once per frame
     void Update()
     {
-        byte[] data = new byte[1024];
-        string input, stringData;
-        IPEndPoint ipep = new IPEndPoint(
-                        IPAddress.Parse("127.0.0.1"), 9024);
-
-        Socket server = new Socket(AddressFamily.InterNetwork,
-                       SocketType.Dgram, ProtocolType.Udp);
-
-
-        string welcome = "Hello, are you there?";
-        data = Encoding.ASCII.GetBytes(welcome);
-        server.SendTo(data, data.Length, SocketFlags.None, ipep);
-
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint Remote = (EndPoint)sender;
-
-        data = new byte[1024];
-        int recv = server.ReceiveFrom(data, ref Remote);
-
-        Console.WriteLine("Message received from {0}:", Remote.ToString());
-        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
-
-        while (true)
+        if(Input.GetKeyDown(KeyCode.Space) && !testing)
         {
-            input = Console.ReadLine();
-            if (input == "exit")
-                break;
-            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
-            data = new byte[1024];
-            recv = server.ReceiveFrom(data, ref Remote);
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
+            testing = true;
+            startThreadingFunction(ExampleClient);
         }
-        Console.WriteLine("Stopping client");
-        server.Close();
+        if(Input.GetKeyDown(KeyCode.Escape) && testing)
+        {
+            Application.Quit();
+        }
+       
     }
 
-    IEnumerator SendingMSG()
-    {
-        byte[] data = new byte[1024];
-        string input, stringData;
-        IPEndPoint ipep = new IPEndPoint(
-                        IPAddress.Parse("127.0.0.1"), 9024);
-        Socket server = new Socket(AddressFamily.InterNetwork,
-                       SocketType.Dgram, ProtocolType.Udp);
-
-
-        string welcome = "ping";
-        data = Encoding.ASCII.GetBytes(welcome);
-        server.SendTo(data, data.Length, SocketFlags.None, ipep);
-
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint Remote = (EndPoint)sender;
-
-        data = new byte[1024];
-        int recv = server.ReceiveFrom(data, ref Remote);
-
-        Console.WriteLine("Message received from {0}:", Remote.ToString());
-        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
-
-        while(true)
-        {
-            input = Console.ReadLine();
-            if (input == "exit")
-                break;
-            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
-            data = new byte[1024];
-            recv = server.ReceiveFrom(data, ref Remote);
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
-
-            yield return null;
-        }
-
-        Console.WriteLine("Stopping client");
-        server.Close();
-    }
+    
+    
 }
--- TCP&UDP/Assets/Scripts/ServerUDP.cs
+++ TCP&UDP/Assets/Scripts/ServerUDP.cs
@@ -5,12 +5,71 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 public class ServerUDP : MonoBehaviour
 {
+    public Socket _socket;
+    int recv;
+    byte[] data = new byte[1024];
+    private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
 
-    private void Start()
+
+    public void Start()
     {
-        
+        startThreadingFunction(Server);
+        //StartCoroutine(Server());
     }
+
+
+    public void startThreadingFunction(Action function)
+    {
+        Thread t = new Thread(function.Invoke);
+        t.Start();
+    }
+
+
+    void Server()
+    {
+        int count = 0;
+        //Create socket
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27000);
+        //Bind Socket to server addres
+        _socket.Bind(ipep);
+
+
+        //wait Until datagram packet arrives from client
+        recv = _socket.ReceiveFrom(data, ref epFrom);
+
+        //when this arrives 
+
+        //we process it and send to the client
+
+        Debug.Log("Welcome to the server " + epFrom.ToString());
+        Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+
+        Thread.Sleep(500);
+        count++;
+        string pong = "pong";
+        data = Encoding.ASCII.GetBytes(pong);
+        _socket.SendTo(data, data.Length, SocketFlags.None, epFrom);
+
+
+        while (count <5 )
+        {
+            count++;
+            data = new byte[1024];
+            recv = _socket.ReceiveFrom(data, ref epFrom);
+            Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+
+            Thread.Sleep(500);
+            data = Encoding.ASCII.GetBytes(pong);
+            _socket.SendTo(data, recv, SocketFlags.None, epFrom);
+        }
+
+        _socket.Close();
+    }
+
 
 }
